43ddf8b9d1c13b75798f494b61dadb2191bf2c44
RajaAnthati <nagaiik@gmail.com>
2016-01-12 18:46:10 +0000

    Shopping Cart Implementation

--- src/CheckOutSystem.scala
+++ src/CheckOutSystem.scala
@@ -1,14 +1,72 @@
+/*
+ * Simple Checkout System 
+ * Pre assumptions :
+ * 1.items fixed
+ * 2.items cost fixed
+ * 3.items checked out cart list is available in List of strings
+ * 4.items case mismatch is handled
+ */
+
 import scala._
+import scala.util._
+import scala.collection.mutable._
+
 
 object CheckOutSystem {
   
-  def  main(args:Array[String]){  
+  
+ 
+   
+  def  main(args:Array[String]){
+                               
+     /*case class items(var apple:String,var price:Double)*/
+     println("..............Welcome to FruitShop.................")
+     println("---------------------------------------------------") 
+     println("----------------ItemsAvailableInStore---------------\n")
+     println("item\t\t"+"cost:\t")
+     printf("1.Apple:(one each)\t"+":60p\n") 
+     printf("2.Orange:(one each)\t"+":25p\n")
     
-  println(".........Welcome to ChekOutSystem ........")
-  case class Message(var message:String)
-  case class  items(val apple:String,val price:Double)
+     var l= List("apple","apple","orange","apple","apple")
+
+     //assuming the list is readily available 
+     var apple_Count=0;
+     var orange_Count=0;
+     var invalid_Items=0;
+     var sum=0.0;
+    if(l.length<0)
+      printf("sorry your cart is... empty try again::")
+      else{
+      l.foreach(x=>if (x.equalsIgnoreCase("Apple")) {
+        apple_Count+=1;
+      }else if(x.equalsIgnoreCase("Orange")){
+        
+        orange_Count+=1;
+      }
+      else{
+        invalid_Items+=1
+        println("Invalid items found.......:"+invalid_Items+"\n")
+        
+      }
+      
+      
+      )
+     
+      }
+    
+    
+  println("Thanks for confirming the shopping cart.........\n")
+  
+  println(" --------you have bought ----------")
+  
+  println("Apples :"+apple_Count+"\t:"+"Oranges:"+orange_Count+"\n")
+  
+   println("Please pay :"+(apple_Count*0.60+orange_Count*0.25)+"pp")
+  
+  
   
   }
-  
+ 
+ 
   
 }
