a2ff2715940b8e0591632bd57ecee99b17c22f14
RajaAnthati <nagaiik@gmail.com>
2016-01-12 16:17:13 +0000

    Initializing project folders

---
+++ src/CheckOutSystem.scala
@@ -0,0 +1,14 @@
+import scala._
+
+object CheckOutSystem {
+  
+  def  main(args:Array[String]){  
+    
+  println(".........Welcome to ChekOutSystem ........")
+  case class Message(var message:String)
+  case class  items(val apple:String,val price:Double)
+  
+  }
+  
+  
+}
